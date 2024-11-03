35d2872a7ff67a4a8946da44d2ce9199b728bc37
Theus Code <133078928+theuscoder@users.noreply.github.com>
2024-11-03 01:17:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+var versenha = document.querySelector('#password');
+var input = document.querySelector('#pass input');
 
+versenha.addEventListener('click', function(){
+    var senha = input.value;
+
+    if (input.getAttribute('type') == 'text') {
+        input.setAttribute('type', 'password');
+        versenha.setAttribute('class', 'fa-regular fa-eye');
+    } else {
+        versenha.setAttribute('class', 'fa-regular fa-eye-slash');
+        input.setAttribute('type', 'text');
+    }
+});
